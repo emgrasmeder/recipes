f395bfcd02fd1658357f13812d70660df2292f30
Em Grasmeder <emily@gras.org>
2025-05-26 21:33:03 +0200

    Create meals.ts

---
+++ meals.ts
@@ -0,0 +1,37 @@
+const meals = {
+  alambre: "alambre" ,
+  cabbage_fried_rice: "cabbage_fried_rice",
+  bolognese: "bolognese",
+  hamburger: "hamburger",
+  salad: "salad",
+  tacos_de_papa: "tacos_de_papa",
+  chili: "chili",
+  tortilla_espanola: "tortilla_espanola",
+
+}
+
+const proteins = {
+  vegan_balls: "vegan_balls",
+  tofu: "tofu",
+  smoked_tofu: "smoked_tofu",
+  seitan: "seitan",
+  soy_protein: "soy_protein",
+  hamburger_meat: "hamburger_meat",
+  lentil: "lentil",
+  bean: "bean",
+  egg: "egg",
+}
+
+const conduits = {
+  bowl: "bowl",
+  durum: "durum",
+  tortilla: "tortilla",
+  plate: "plate",
+}
+
+const vegetables = {
+  mushroom: "mushroom",
+  pepper: "pepper",
+  onion: "onion",
+  papa: "papa",
+}
